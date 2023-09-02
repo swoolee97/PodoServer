6196fb8cc1dfa51ab33c83ac456627d6859a6d8f
조성주 <joseongju@joseongjuui-MacBookAir.local>
2023-09-02 20:47:45 +0900

    feed update

--- routes/feed.js
+++ routes/feed.js
@@ -2,24 +2,58 @@ const express = require("express");
 const router = express.Router();
 const Feed = require('../models/Feed');
 const verifyAccessToken = require('../middleware/verifyingToken');
+const aws = require('aws-sdk');
+const multer = require('multer')
+const multerS3 = require('multer-s3');
 
+aws.config.update({
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    region: 'ap-northeast-2'
+});
 
+const s3 = new aws.S3();
+
+const upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: 'feedpodo',
+      key: function (req, file, cb) {
+        cb(null, Date.now().toString() + '-' + file.originalname);
+      }
+    })
+  });
+  
+  
 router.post('/posts', verifyAccessToken, upload.array('image'), async (req, res) => {
     try {
-        const email = req.user_email;
-        const { text, imageUrl } = req.body;
+        const email = req.headers['user_email'];
+        const text = req.body.text;
+    
+        // 이미지 처리
+        const images = req.files;
+        const imageUrls = images.map(file => file.location);
+    
+        console.log("Image URLs: ", imageUrls);
 
-        // 새로운 피드 객체를 생성
-        const newPost = new Feed({ email, text, imageUrl });
-
+        // 새로운 피드 객체 생성
+        const newPost = new Feed({ email, text, imageUrl: imageUrls });
+    
         // 데이터베이스에 저장
         await newPost.save();
 
         // 클라이언트에게 응답
-        res.status(201).json(newPost);
+        res.status(201).json({
+            success: true, // successfully created a post
+            message: '포스트가 성공적으로 생성되었습니다.',
+            post: newPost  // you can still send the newPost object if needed
+        });
     } catch (error) {
         console.error("Error in POST /posts:", error);
-        res.status(500).json({ message: 'Internal Server Error' });
+        res.status(500).json({
+            success: false, // indicates that the operation was not successful
+            message: 'Internal Server Error'
+        });
     }
 });
 
