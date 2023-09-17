d3766699fa195b85fd5fccc8d99598e93864ebe8
swoolee97 <swoolee97@ajou.ac.kr>
2023-09-17 16:23:48 +0900

    포인트 리스트

--- routes/point.js
+++ routes/point.js
@@ -26,4 +26,29 @@ router.get('/sum', async (req, res) => {
     }
 });
 
+router.get('/list', async (req,res)=>{
+    const email = req.query.email;
+    const page = req.query.page;
+    const limit = 10;
+    let skip = (page-1)*limit;
+    try {
+        const points = await Point.find({
+            email: email
+        })
+            .sort({ _id: -1 }) // 내림차순
+            .skip(skip)
+            .limit(limit);
+        const hasMore = points.length === limit;
+        console.log(points)
+        if (points.length === 0) {//
+            return res.status(201).json({ points: [], hasMore: hasMore });
+        }
+        return res.status(200).json({ points: points, hasMore: hasMore });
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ message: 'error', hasMore: hasMore })
+    }
+
+})
+
 module.exports = router;
